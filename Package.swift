898e6accd746347643edcaf203b9892b94839b5e
Nastassia Makaranka <nastassia.makaranka@mapbox.com>
2023-11-20 14:16:14 +0100

    Use snapshot

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 import Foundation
 
-let version = "11.0.0-rc.2"
-let checksum = "3b336739f93cf5dce6fadea11b99b31f249df0f22711d0d8b61f358bfe10a5ca"
+let version = "11.0.0-rc.1"
+let checksum = "38937e85667c090c486cae76f20ee37c16435fb21d2f0af1c54079f55e26ca5c"
 
 let package = Package(
     name: "MapboxCoreMaps",
@@ -16,12 +16,15 @@ let package = Package(
             targets: ["MapboxCoreMapsWrapper"]),
     ],
     dependencies: [
-        .package(name: "MapboxCommon", url: "https://github.com/mapbox/mapbox-common-ios.git", from: "24.0.0-rc.3"),
+        .package(url: "https://github.com/kried/mapbox-common-ios.git", revision: "df75e5eec9c60db7980da7379dd20a25bca2a315"),
     ],
     targets: [
         .target(
             name: "MapboxCoreMapsWrapper",
-            dependencies: ["MapboxCommon", "MapboxCoreMaps"]
+            dependencies: [
+                .product(name: "MapboxCommon", package: "mapbox-common-ios"), 
+                "MapboxCoreMaps"
+            ]
         ),
         .binaryTarget(
             name: "MapboxCoreMaps",
